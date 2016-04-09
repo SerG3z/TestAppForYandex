f169673ddf8af334c716a52d970398838c6ca266
serg <serg3z.hard@gmail.com>
2016-04-09 23:16:52 +0600

    added parcelable

--- app/src/main/java/com/example/serg/testwork/models/Artist.java
+++ app/src/main/java/com/example/serg/testwork/models/Artist.java
@@ -1,11 +1,14 @@
 package com.example.serg.testwork.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.List;
 
 /**
  * Created by serg on 09.04.16.
  */
-public class Artist {
+public class Artist implements Parcelable {
     private int id;
     private String name;
     private String[] genres;
@@ -46,4 +49,44 @@ public class Artist {
     public Cover getCover() {
         return cover;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(name);
+        parcel.writeStringArray(genres);
+        parcel.writeInt(tracks);
+        parcel.writeInt(albums);
+        parcel.writeString(link);
+        parcel.writeString(description);
+        parcel.writeParcelable(cover, 1);
+    }
+
+    protected Artist(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        genres = in.createStringArray();
+        tracks = in.readInt();
+        albums = in.readInt();
+        link = in.readString();
+        description = in.readString();
+        cover = in.readParcelable(Cover.class.getClassLoader());
+    }
+
+    public static final Creator<Artist> CREATOR = new Creator<Artist>() {
+        @Override
+        public Artist createFromParcel(Parcel in) {
+            return new Artist(in);
+        }
+
+        @Override
+        public Artist[] newArray(int size) {
+            return new Artist[size];
+        }
+    };
 }
--- app/src/main/java/com/example/serg/testwork/models/Cover.java
+++ app/src/main/java/com/example/serg/testwork/models/Cover.java
@@ -1,9 +1,12 @@
 package com.example.serg.testwork.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by serg on 09.04.16.
  */
-public class Cover {
+public class Cover implements Parcelable {
     private String small;
     private String big;
 
@@ -14,4 +17,32 @@ public class Cover {
     public String getBig() {
         return big;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(small);
+        parcel.writeString(big);
+    }
+
+    protected Cover(Parcel in) {
+        small = in.readString();
+        big = in.readString();
+    }
+
+    public static final Creator<Cover> CREATOR = new Creator<Cover>() {
+        @Override
+        public Cover createFromParcel(Parcel in) {
+            return new Cover(in);
+        }
+
+        @Override
+        public Cover[] newArray(int size) {
+            return new Cover[size];
+        }
+    };
 }
