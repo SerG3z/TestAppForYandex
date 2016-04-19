cfa2408ca976e7cffa60641786ff4dc323a6d8fb
serg3z <serg3z.hard@gmail.com>
2016-04-19 17:36:07 +0600

    take out repeating code

---
+++ app/src/main/java/com/example/serg/testwork/activity/BaseActivity.java
@@ -0,0 +1,29 @@
+package com.example.serg.testwork.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by serg3z on 19.04.16.
+ *
+ * This Activity contains repeating code
+ */
+
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ButterKnife.unbind(this);
+    }
+}
